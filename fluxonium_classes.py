572265929f4b787001b106794345e7d8cc87ec1b
Tyra Samenius <cortinez@chalmers.se>
2026-07-04 18:17:58 +0200

    added functions to calculate Purcell limited T1

--- fluxonium_classes.py
+++ fluxonium_classes.py
@@ -244,6 +244,38 @@ class Fluxonium(Device):
         plt.xlabel(r'$\phi_e$')
         plt.ylabel(r'$\chi/2\pi$ (MHz)')
         return chis, frs, fqs
+    
+        
+    def Purcell_lim_T1_analytical(self, res_ind, phi_e, kappa_r):
+        H_effs = self.get_h_effs(phi_e)
+        H_eff = H_effs[res_ind]
+        eigenenergies, eigenstates = H_eff.eigenstates(eigvals=4)
+        f_q = eigenenergies[1] - eigenenergies[0]
+
+        res = self.resonators[res_ind]
+        f_r = res.freq
+
+        delta = f_r - f_q
+
+        g_charge_charge = self.res_coupling_strengths[res_ind]
+        g = g_charge_charge * ( self.get_n_zpf(phi_e) * res.get_n_zpf())
+
+        gamma = kappa_r*np.abs(g/delta)**2
+        T1 = 1/gamma * 10 ** (-6) # ms
+        return T1
+    
+    def Purcell_lim_T1_sim(self, res_ind, phi_e, kappa_r):
+        H = self.get_coupled_hamiltonian(res_ind, phi_e)
+        res = self.resonators[res_ind]
+        
+        a_op = tensor(qeye(self.N_phi), destroy(res.fock_dim))
+        eigenenergies, eigenstates = H.eigenstates(eigvals=4)
+        g_state = eigenstates[0]
+        e_state = eigenstates[1] # assuming f_q < f_r
+        gamma = kappa_r*np.abs(g_state.overlap(a_op * e_state))**2
+        
+        T1 = 1/gamma
+        return T1 * 10 ** (-6) # ms
 
     def get_charge_op(self):
         N_phi = self.N_phi
@@ -261,7 +293,6 @@ class Fluxonium(Device):
         e_state = eigenstates[1]
 
         n_zpf = np.abs(g_state.overlap(n_charge_op*e_state))
-
         return n_zpf
     
     def set_E_C_effs(self, E_C_effs):
